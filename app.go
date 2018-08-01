9c7af73fc9c108afbc72b6a4c721ec0ff68f20b6
mlctrez <mlctrez@gmail.com>
2018-07-31 22:41:16 -0500

    update message

--- app.go
+++ app.go
@@ -11,7 +11,7 @@ func main() {
 
 	router := web.New(Context{})
 	router.Get("/", func(w web.ResponseWriter, req *web.Request) {
-		w.Write([]byte("hello world"))
+		w.Write([]byte("hello world 2"))
 	})
 	http.ListenAndServe(":80", router)
 }
