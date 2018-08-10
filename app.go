2ca3780cd082c726a565810b7a49cc23d062fe92
Matt Crawford <mlctrez@gmail.com>
2018-08-10 10:42:21 -0500

    move db to top level

--- app.go
+++ app.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"database/sql"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/go-sql-driver/mysql"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gocraft/web"
 )
@@ -26,24 +30,29 @@ func main() {
 
 		w.Header().Set("Content-Type", "text/plain")
 
-		//rootCAs := x509.NewCertPool()
-		//
-		//pem, err := ioutil.ReadFile("/rds-combined-ca-bundle.pem")
-		//if err != nil {
-		//	panic(err)
-		//}
-		//if ok := rootCAs.AppendCertsFromPEM(pem); !ok {
-		//	panic(err)
-		//}
-		//
-		//mysqlHost := os.Getenv("MYSQL_HOST")
-		//mysql.RegisterTLSConfig("custom", &tls.Config{
-		//	RootCAs:    rootCAs,
-		//	ServerName: mysqlHost,
-		//})
-		//con := fmt.Sprintf("%s:%s@tcp(%s:3306)/mysql?tls=custom", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), mysqlHost)
+		var con string
 
-		con := fmt.Sprintf("%s:%s@tcp(%s:3306)/mysql", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"))
+		if false {
+			rootCAs := x509.NewCertPool()
+
+			pem, err := ioutil.ReadFile("/rds-combined-ca-bundle.pem")
+			if err != nil {
+				panic(err)
+			}
+			if ok := rootCAs.AppendCertsFromPEM(pem); !ok {
+				panic(err)
+			}
+
+			mysqlHost := os.Getenv("MYSQL_HOST")
+			config := &tls.Config{
+				RootCAs:    rootCAs,
+				ServerName: "",
+			}
+			mysql.RegisterTLSConfig("custom", config)
+			con = fmt.Sprintf("%s:%s@tcp(%s:3306)/mysql?tls=custom", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), mysqlHost)
+		} else {
+			con = fmt.Sprintf("%s:%s@tcp(%s:3306)/mysql", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"))
+		}
 
 		db, err := sql.Open("mysql", con)
 		if err != nil {
--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
 module github.com/mlctrez/ecs-demo-php-simple-app
 
 require (
-	github.com/davecgh/go-spew v1.1.0
+	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.4.0
 	github.com/gocraft/web v0.0.0-20170925135945-d8611de039df
-	github.com/pmezard/go-difflib v1.0.0
-	github.com/stretchr/testify v1.2.2
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
+	google.golang.org/appengine v1.1.0 // indirect
 )
